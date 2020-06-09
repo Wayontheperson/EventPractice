c304f7a2f8315a5f96a4186f8b5dae85818451cc
Park.J.H <ntcf652@gmail.com>
2020-06-09 17:51:32 +0900

    Initial commit

---
+++ EventPractice/Peson.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+
+namespace EventPractice
+{
+    public class Peson
+    {
+        private int _age;
+
+        public int Age { get; set; }
+        
+        private string _name;
+        
+        public string _Name { get; set; }
+
+        public Peson(int age, string name)
+        {
+            _age = age;
+            _name = name;
+        }
+
+        // public void IncreaseAge()
+        // {
+        //     _age++;
+        //     Console.WriteLine($"{oldage} -> {_age} ");
+        // }
+
+        public delegate void AgeChangedDelgate(int oldage, int newAge);
+
+        public AgeChangedDelgate AgeChanged;
+
+        public void IncreaseAge()
+        {
+            int oldage = _age;
+            _age++;
+            if (AgeChanged != null)
+                AgeChanged(oldage, _age);
+        }
+
+        public event AgeChangedEventHandler AgeChanged;
+        
+        public void 
+
+
+    }
+}
---
+++ EventPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EventPractice
+{
+    class Program
+    {
+         //이럴경우 instant가 static에 접근하는 것이 아닌
+                                //private static void Person_AgeChanged가 int birth에 접근하는것
+                                //즉 static->instant 인 상황 에러 없애려면 
+                                //private static int birth
+                                //private  void Person_AgeChanged(int oldage, int newage)
+                                //으로 바꿔야 되는데 이러면 private  void Person_AgeChanged(int oldage, int newage)을 메인함수가 못읽음
+        static void Main(string[] args)
+        {
+            Peson peson = new Peson(32,"김과장");
+           // peson.AgeChanged=new Peson.AgeChangedDelgate(Person_AgeChanged);
+           peson.AgeChanged = Person_AgeChanged; //new AgechangedDelegate(Person_AgeChanged)
+           peson.IncreaseAge();
+        }
+    
+        private static void Person_AgeChanged(int oldage, int newage)
+        {
+            int birth = 11;
+            // int birthYear = 1976;
+            Console.WriteLine($"{oldage} -> {newage} : {birth}년생 ");
+        }
+    }
+}
